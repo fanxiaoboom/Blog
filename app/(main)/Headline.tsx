d992430916e42998539a28be9f7796eacd7d096d
fanxiaoboom <fanxiaoboom@gmail.com>
2024-12-07 11:39:35 +0800

    reset

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -4,13 +4,12 @@ import { motion } from 'framer-motion'
 import Balancer from 'react-wrap-balancer'
 
 import { SparkleIcon, UserSecurityIcon } from '~/assets'
-import { PeekabooLink } from '~/components/links/PeekabooLink'
 import { SocialLink } from '~/components/links/SocialLink'
 
 function Developer() {
   return (
     <span className="group">
-      <span className="font-mono">&lt;</span>开发者
+      <span className="font-mono">&lt;</span>产品经理
       <span className="font-mono">/&gt;</span>
       <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
     </span>
@@ -26,7 +25,7 @@ function Designer() {
         <span className="absolute -bottom-[3.5px] -left-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
         <span className="absolute -right-[3.5px] -top-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
       </span>
-      设计师
+      体验设计师
     </span>
   )
 }
@@ -44,7 +43,7 @@ function Founder() {
   return (
     <span className="group inline-flex items-center">
       <UserSecurityIcon className="mr-1 inline-flex group-hover:fill-zinc-600/20 dark:group-hover:fill-zinc-200/20" />
-      <span>创始人</span>
+      <span>高兴</span>
     </span>
   )
 }
@@ -80,10 +79,7 @@ export function Headline() {
         }}
       >
         <Balancer>
-          我是 Cali，
-          <PeekabooLink href="https://zolplay.com">佐玩</PeekabooLink>
-          创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。
-          我热爱开发，设计，创新，享受生活，以及在未知领域中探索。
+          我是 Bran，这是在试试BooMooSpace的博客。
         </Balancer>
       </motion.p>
       <motion.div
--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,13 +1,9 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import type { Image } from 'sanity'
+import { SanityImageSource } from "@sanity/image-url/lib/types/types"
+import { dataset, projectId } from '../env'
 
-import { dataset, projectId } from '~/sanity/env'
+const builder = createImageUrlBuilder({ projectId, dataset })
 
-const imageBuilder = createImageUrlBuilder({
-  projectId: projectId || '',
-  dataset: dataset || '',
-})
-
-export const urlForImage = (source: Image) => {
-  return imageBuilder?.image(source).auto('format').fit('max')
+export const urlForImage = (source: SanityImageSource) => {
+  return builder.image(source)
 }
